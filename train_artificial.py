b94186fdaf9124f4522ce4f87003224b33f9637a
Amjed1234567 <amjed.afzal@hotmail.com>
2025-10-04 12:37:16 +0200

    LR=5e-1.

--- train_artificial.py
+++ train_artificial.py
@@ -15,7 +15,7 @@ import baseline
 DATA_SIZE        = 20000
 BATCH_SIZE       = 256
 EPOCHS           = 50
-LR               = 1e-2
+LR               = 5e-1
 VAL_FRAC         = 0.10
 TEST_FRAC        = 0.10
 SEED             = 0
