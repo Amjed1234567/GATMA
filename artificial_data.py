20749f3c42835ec4efca58fbc2f096ea774f4f01
Amjed1234567 <amjed.afzal@hotmail.com>
2025-10-05 14:43:37 +0200

    Also negative random values.

--- artificial_data.py
+++ artificial_data.py
@@ -4,14 +4,14 @@ import torch
 
 def create_one_sample():
     # Plane parameters.
-    a = rnd.randrange(1,10, 1)
-    b = rnd.randrange(1,10, 1)
-    c = rnd.randrange(1,10, 1)
-    d = rnd.randrange(1,10, 1)
+    a = rnd.randrange(-10,10, 1)
+    b = rnd.randrange(-10,10, 1)
+    c = rnd.randrange(-10,10, 1)
+    d = rnd.randrange(-10,10, 1)
     # Point parameters.
-    x = rnd.randrange(1,10, 1)
-    y = rnd.randrange(1,10, 1)
-    z = rnd.randrange(1,10, 1)
+    x = rnd.randrange(-10,10, 1)
+    y = rnd.randrange(-10,10, 1)
+    z = rnd.randrange(-10,10, 1)
     # Euclidian distance between point and plane.
     dist = abs(a*x + b*y + c*z + d) / math.sqrt(a**2 + b**2 + c**2)
 
